e2b0ca57bdc55ca0719bbea6644a2107ae309256
fredy estiven <fredyestivendiazpinila3@gmail.com>
2025-09-15 20:36:02 -0500

    creacion del controlador

--- config/database.php
+++ config/database.php
@@ -3,5 +3,7 @@ class database{
     public static function conectar(){
         $conexion = new mysqli("localhost","root","tienda","","tienda2");
         $conexion->query("SET NAMES 'utf-8'");
+
+        return $conexion;
     }
 }
